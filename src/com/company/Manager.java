05447a85da3c0de01a9628f37a4126e2012c15c8
sheilalopez <47568080+sheilalopez@users.noreply.github.com>
2019-03-21 11:59:04 +0100

    sigo

--- src/com/company/Manager.java
+++ src/com/company/Manager.java
@@ -1,4 +1,49 @@
 package com.company;
 
-public class Manager {
+import java.util.LinkedList;
+import java.util.List;
+
+public class Manager extends Employee {
+    //atributos
+    private List<Employee> employees;
+
+    public void addEmployeeToManager (Employee e){
+        this.employees.add(e);
+    }
+    public int getListSize(){
+        return employees.size();
+    }
+    public List<Employee> getEmployees(){
+        return employees;
+    }
+    public Manager (String id, String name, double salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.employees = new LinkedList<>();
+
+    }
+    @Override
+    public void updateSalary(double base_salary) {
+
+        double sell_amount = 0;
+        double bonus;
+
+        for(Employee employee: employees){
+            if(employee instanceof Vendedor){
+                List<Sell> sells = ((Vendedor) employee).getSells();
+
+                if(sells!=null) {
+                    for (Sell sell : sells) {
+                        sell_amount += sell.getAmount();
+                    }
+                }
+            }
+        }
+
+        bonus = sell_amount/100;
+
+        this.salary = base_salary + bonus;
+    }
+
 }
